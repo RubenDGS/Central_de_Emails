8c9ff52d9086345ec9d2d12b48c1242c79fc55ac
RubenDGS <ruben-santos-20@hotmail.com>
2026-08-11 11:45:11 +0100

    Refactor DEFAULT_ACCOUNTS for improved formatting
    Formatted account entries for better readability.

--- app/src/main/java/pt/caixa6/Account.kt
+++ app/src/main/java/pt/caixa6/Account.kt
@@ -7,10 +7,39 @@ data class Account(
 )
 
 val DEFAULT_ACCOUNTS = listOf(
-    Account("rita_sapo", "Rita Sapo", "https://mail.sapo.pt/"),
-    Account("pai_sapo", "Pai Sapo", "https://mail.sapo.pt/"),
-    Account("leonor_sapo", "Leonor Sapo", "https://mail.sapo.pt/"),
-    Account("daniela_sapo", "Daniela Sapo", "https://mail.sapo.pt/"),
-    Account("mae_sapo", "Mãe Sapo", "https://mail.sapo.pt/"),
-    Account("rita_gmail", "Rita Gmail", "https://mail.google.com/")
+    Account(
+        "rita_sapo",
+        "Rita Sapo",
+        "https://mail.sapo.pt/"
+    ),
+
+    Account(
+        "pai_sapo",
+        "Pai Sapo",
+        "https://mail.sapo.pt/"
+    ),
+
+    Account(
+        "leonor_sapo",
+        "Leonor Sapo",
+        "https://mail.sapo.pt/"
+    ),
+
+    Account(
+        "daniela_sapo",
+        "Daniela Sapo",
+        "https://mail.sapo.pt/"
+    ),
+
+    Account(
+        "mae_sapo",
+        "Mãe Sapo",
+        "https://mail.sapo.pt/"
+    ),
+
+    Account(
+        "rita_gmail",
+        "Rita Gmail",
+        "https://mail.google.com/"
+    )
 )
